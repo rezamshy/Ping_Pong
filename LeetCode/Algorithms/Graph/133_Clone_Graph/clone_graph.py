7f2b1554ca8213ca4f2d90cf649533752994ed6f
Reza Mashayekhi <rezamashayekhi22@yahoo.com>
2023-05-03 23:04:51 -0600

    initial graph code

---
+++ LeetCode/Algorithms/Graph/133_Clone_Graph/clone_graph.py
@@ -0,0 +1,23 @@
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+
+
+class Solution(object):
+    def cloneGraph(self, node):
+        """
+        :type node: Node
+        :rtype: Node
+        """
+    def readGraph(self, s):
+        
+
+def main():
+    s = input()
+    solution.readGraph()
+
+
+if __name__ == "__main__":
+    main()
