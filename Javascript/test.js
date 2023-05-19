0f8410cc608e96e56384c9902876d8a0a5eb0e5e
Reza Mashayekhi <rezamashayekhi22@yahoo.com>
2023-05-19 00:01:06 -0600

    Add python file for testig shallow copy

--- Javascript/test.js
+++ Javascript/test.js
@@ -1,1 +1,8 @@
-document.write("This is written by JS.")
+let sum = 0; 
+const a = [1, 2, 3];
+a.forEach(getSum);
+function getSum(ele) {
+   sum += ele;
+}
+//console.log(person.getName());
+document.write("This is written by JS. " + sum)
---
+++ Python/copy.py
@@ -0,0 +1,18 @@
+import copy
+
+old_list = [[1, 1, 1], [2, 2, 2], [3, 3, 3]]
+new_list = copy.copy(old_list)
+
+old_list.append([4, 4, 4])
+
+print("Old list:", old_list)
+print("New list:", new_list)
+
+list1 = [1, 2, [3, 4]]
+list2 = copy.copy(list1)
+
+list2[0] = 5
+list2[2][0] = 6
+
+print(list1)  # Output: [1, 2, [6, 4]]
+print(list2)  # Output: [5, 2, [6
